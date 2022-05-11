0e63e23f78ae574457370746deeace3f0323d8f2
ADCL <william.pope@colorado.edu>
2022-05-11 11:27:29 -0600

    updated orientation in controller.jl

--- controller/src/controller.jl
+++ controller/src/controller.jl
@@ -4,7 +4,7 @@
 
 using RobotOS
 using JLD
-using Rotations
+using ReferenceFrameRotations
 
 include("/home/adcl/Documents/marmot-algs/HJB-planner/HJB_functions.jl")
 
@@ -56,24 +56,25 @@ function main()
     spin()
 end
 
-# ISSUE: not being called -> subscriber not working? spin() blocking incorrectly?
-function run_control(pose_msg::PoseStamped, ctrl_pub, U_HJB, sg, veh)
-    println("run_control()")
-    
+# ISSUE: not sure if action is correct for pose
+#   - need to make sure quat -> euler is correct
+function run_control(pose_msg::PoseStamped, ctrl_pub, U_HJB, sg, veh)    
     dt = 0.1
     
     y = zeros(3)
     y[1] = pose_msg.pose.position.x
     y[2] = pose_msg.pose.position.y
 
-    println(y)
+    # ISSUE: think theta angle is based off y-axis, not x-axis (just add pi/2)
+    # TO-DO: want to move this (pose->state) to state_estimator() node
+    ori = pose_msg.pose.orientation
+    quat = ReferenceFrameRotations.Quaternion(ori.w, ori.x, ori.y, ori.z);
+    eul = ReferenceFrameRotations.quat_to_angle(quat, :XYZ)
+    y[3] = eul.a3
 
-    # ISSUE: orientation is a 4 part (x,y,z,w) output, need to split into theta
-    y[3] = pose_msg.pose.orientation    # ISSUE
+    println("y: ", y)
 
-    println(y)
-
-    # To-DO: implement new structure to get rid of this
+    # TO-DO: implement new structure to get rid of this
     T_xy_set = [[-0.5 4.0];
                 [0.5 4.0];
                 [0.5 5.0];
@@ -81,16 +82,17 @@ function run_control(pose_msg::PoseStamped, ctrl_pub, U_HJB, sg, veh)
 
     T_theta_set = [[-pi, pi]]
 
-
     if in_target_set(y, T_xy_set, T_theta_set, veh) == false
         a = optimal_action_HJB(y, U_HJB, sg, veh)
     else
         a = [0.0, 0.0]
     end
 
+    println("a: ", a)
     publish_ctrl(ctrl_pub, a, dt)
 end
 
+# ISSUE: error is in here
 function publish_ctrl(ctrl_pub, ctrl, dt)
     dur = Duration(dt)
     rate = Rate(10)
@@ -99,8 +101,8 @@ function publish_ctrl(ctrl_pub, ctrl, dt)
     ctrl_msg = AckermannDriveStamped()
     ctrl_msg.header.stamp = get_rostime()
 
-    ctrl_msg.drive.speed = parse(Float64, ctrl[1])
-    ctrl_msg.drive.steering_angle = parse(Float64, ctrl[2])
+    ctrl_msg.drive.speed = ctrl[1]
+    ctrl_msg.drive.steering_angle = ctrl[2]
 
     while get_rostime()-start < dur
         publish(ctrl_pub, ctrl_msg)
